0e86ee4dc3d57dce0c0db33c359934ff747c2785
Tyler Negro <TylerNegro@Tylers-MBP.lan>
2018-04-16 17:40:20 -0600

    added files

---
+++ vscode.js
@@ -0,0 +1,44 @@
+{
+  "editor.fontSize": 16,
+  "editor.minimap.enabled": false,
+  "emmet.syntaxProfiles": {
+    "javascript": "jsx"
+  },
+  "editor.wordWrap": "on",
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact",
+  },
+  "emmet.triggerExpansionOnTab": true,
+  "workbench.iconTheme": "material-icon-theme",
+  "editor.tabSize": 2,
+  "liveServer.settings.CustomBrowser": "chrome",
+  "window.zoomLevel": 0,
+  "workbench.colorTheme": "Cobalt2",
+  "telemetry.enableTelemetry": false,
+  //theme settings
+  "editor.fontFamily": " 'Operator Mono','Inconsolata for Powerline', Menlo, Monaco, 'Courier New', monospace",
+  "editor.lineHeight": 25,
+  "editor.letterSpacing": 0.5,
+  "files.trimTrailingWhitespace": true,
+  "editor.fontWeight": "400",
+  "editor.cursorStyle": "line",
+  "editor.cursorWidth": 2,
+  "editor.cursorBlinking": "smooth",
+  // Very important: Install this plugin: https://github.com/be5invis/vscode-custom-css
+  // you'll need to change this to an absolute path on your computer
+  "vscode_custom_css.imports": [
+    "/Users/TylerNegro/.vscodestyles.css"
+  ],
+  "editor.renderWhitespace": "all",
+  "editor.fontLigatures": true,
+  // ================================
+  // eslint configs
+  "editor.formatOnSave": true,
+  "[javascript]": {
+    "editor.formatOnSave": false
+  },
+  "eslint.autoFixOnSave": true,
+  "eslint.alwaysShowStatus": true,
+  "terminal.integrated.shell.osx": "zsh"
+  // ================================
+}
